cheeseywhiz/album-match

server: Add Currently Playing Album to users table

server/tests/conftest.py
@@ -33,12 +33,16 @@ def fresh_db(runner):
     vitals.db.close_db()
 
 
-def user_client(username, password, app):
+def user_client(username, password, app, setup=False):
     with app.test_client() as client:
         with app.app_context():
             url = flask.url_for('user.user_login')
             response = client.post(url, json=dict(username=username, password=password))
             assert response.status_code == 200
+
+            if setup:
+                yield client
+                # do more setup then pass back
 
         yield client
 
@@ -51,3 +55,13 @@ def testuser_client(app):
 @pytest.fixture
 def emptyuser_client(app):
     yield from user_client('emptyuser', 'empty', app)
+
+
+@pytest.fixture
+def current_album_client(app):
+    client_manager = user_client('testuser', 'password', app, setup=True)
+    client = next(client_manager)
+    url = flask.url_for('user.user_album', catalog='TPLP101')
+    request = client.post(url)
+    assert request.status_code == 200
+    yield from client_manager

cheeseywhiz/vitals

server/tests/conftest.py
@@ -33,12 +33,16 @@ def fresh_db(runner):
     vitals.db.close_db()
 
 
-def user_client(username, password, app):
+def user_client(username, password, app, setup=False):
     with app.test_client() as client:
         with app.app_context():
             url = flask.url_for('user.user_login')
             response = client.post(url, json=dict(username=username, password=password))
             assert response.status_code == 200
+
+            if setup:
+                yield client
+                # do more setup then pass back
 
         yield client
 
@@ -51,3 +55,13 @@ def testuser_client(app):
 @pytest.fixture
 def emptyuser_client(app):
     yield from user_client('emptyuser', 'empty', app)
+
+
+@pytest.fixture
+def current_album_client(app):
+    client_manager = user_client('testuser', 'password', app, setup=True)
+    client = next(client_manager)
+    url = flask.url_for('user.user_album', catalog='TPLP101')
+    response = client.post(url)
+    assert response.status_code == 200
+    yield from client_manager